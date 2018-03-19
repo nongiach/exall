67b5530e026d7b51f23bf582492ef67f6eb077ba
chaign_c <betsizaraina.chaignon@epitech.eu>
2018-03-19 01:05:09 +0100

    push files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, Extension
+
+setup(name='exall',
+        version='1.0',
+        author='@chaign_c',
+        url='https://www.python.org/sigs/distutils-sig/',
+        py_modules= ['exall']
+        )
+# py_modules=['exall/'],
