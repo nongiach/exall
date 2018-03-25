dd825df9825f13b07e9c0a06004decf5d43c9c2d
chaign_c <betsizaraina.chaignon@epitech.eu>
2018-03-25 13:37:45 +0200

    push 0.11 version

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, Extension
 
 setup(name='exall',
-        version='0.1',
+        version='0.11',
         author='@chaign_c',
         author_email = 'chaignc@hexpresso.fr',
         description = """Exall is an exception manager based on decorator/context/callback.  https://github.com/nongiach/exall/""",
-        url='https://github.com/nongiach/exall/archive/0.1.tar.gz',
+        url='https://github.com/nongiach/exall/',
         py_modules= ['exall'],
         keywords=['exception', 'except', 'callback', 'raise', 'python']
         )
+# url='https://github.com/nongiach/exall/archive/0.1.tar.gz',
